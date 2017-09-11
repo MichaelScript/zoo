725aba5c8a6dc52676c0f0a00592ceb36357ad43
Michael Green <me@michaelgreen.net>
2017-09-11 15:43:37 -0700

    python app

--- zoo-backend/app/start_server.py
+++ zoo-backend/app/start_server.py
@@ -1,1 +1,22 @@
-print("Hello world")
+#!/usr/bin/env python
+import werkzeug
+from flask import Flask,request,abort,jsonify
+from flask_restful import Resource,Api
+
+app = Flask(__name__)
+api = Api(app)
+
+class Animals(Resource):
+    def get(self):
+        return {
+        	"species":"monkey",
+        	"age":"1337"
+        }
+
+api.add_resource(Animals,"/animals/")
+
+def main():
+    app.run(host='0.0.0.0',port=6666,debug=True)
+
+if __name__ == '__main__':
+    main()
